4c77f5567c9315ad518e0e88bb1f60f370d636e9
arinach <arina.tchukurova@yandex.ru>
2022-08-20 01:18:40 +0300

    add dljgjdlg

---
+++ sky/Program.cs
@@ -0,0 +1,13 @@
+﻿int x;
+Console.WriteLine("Введи первое число - ");
+x = Convert.ToInt32(Console.ReadLine());
+
+int y;
+Console.WriteLine("Введи второе число - ");
+y = Convert.ToInt32(Console.ReadLine());
+
+int z;
+Console.WriteLine("Введи и третье число - ");
+z = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Math.Max(x, Math.Max(y, z)));
